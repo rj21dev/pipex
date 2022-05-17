c3edba9f4715ca29b96468d63c29b2c73ddf2e14
Rennifer Jada <rjada@student.21-school.ru>
2022-05-17 23:40:55 +0300

    add forks

--- srcs/main.c
+++ srcs/main.c
@@ -1,4 +1,6 @@
 #include "../libft/libft.h"
+#include <fcntl.h>
+#include <sys/wait.h>
 #include <stdio.h>
 
 char	**fill_paths(char **envp)
@@ -22,7 +24,7 @@ char	**fill_paths(char **envp)
 	return (NULL);
 }
 
-void	update_paths(char **argv, char **paths)
+void	update_paths(char *arg, char **paths)
 {
 	int	i;
 
@@ -33,7 +35,7 @@ void	update_paths(char **argv, char **paths)
 		tmp = ft_strjoin(paths[i], "/");
 		free(paths[i]);
 		paths[i] = tmp;
-		tmp = ft_strjoin(paths[i], argv[1]);
+		tmp = ft_strjoin(paths[i], arg);
 		free(paths[i]);
 		paths[i] = tmp;
 		i++;
@@ -68,17 +70,59 @@ void	print_paths(char **paths)
 
 int	main(int argc, char **argv, char **envp)
 {
-	(void)argc;
-	//(void)argv;
+	pid_t	p1;
+	pid_t	p2;
+	int	infile;
+	int	outfile;
+	int	fd[2];
+	char	**args;
 	char	**paths;
 	char	*true_path;
 
-	paths = fill_paths(envp);
-	update_paths(argv, paths);
-	true_path = get_real_path(paths);
-	printf("%s\n", true_path);
-	//print_paths(paths);
-	free(true_path);
-	ft_split_free(paths);
+	if (argc != 5)
+		return (1);
+	infile = open(argv[1], O_RDONLY);
+	outfile = open(argv[4], O_WRONLY, O_CREAT | O_CREAT, 0644);
+	pipe(fd);
+	p1 = fork();
+	if (p1 == 0)
+	{
+		dup2(fd[1], STDOUT);
+		dup2(infile, STDIN);
+		paths = fill_paths(envp);
+		update_paths(argv[2], paths);
+		args = ft_split(argv[2], ' ');
+		true_path = get_real_path(paths);
+		close(fd[0]);
+		close(fd[1]);
+		close(infile);
+		execve(true_path, args, envp);
+		free(true_path);
+		ft_split_free(paths);
+		ft_split_free(args);
+	}
+	p2 = fork();
+	if (p2 == 0)
+	{
+		dup2(fd[0], STDIN);
+		dup2(outfile, STDOUT);
+		paths = fill_paths(envp);
+		update_paths(argv[3], paths);
+		args = ft_split(argv[3], ' ');
+		true_path = get_real_path(paths);
+		close(fd[0]);
+		close(fd[1]);
+		close(outfile);
+		execve(true_path, args, envp);
+		free(true_path);
+		ft_split_free(paths);
+		ft_split_free(args);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	close(infile);
+	close(outfile);
+	waitpid(p1, NULL, 0);
+	waitpid(p2, NULL, 0);
 	return (0);
 }
